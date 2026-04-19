3853cd320c40e9335b7de9450ffcf3410d13a11d
houshanyun <likeredbean@gmail.com>
2026-04-19 19:47:15 +0800

    docs: 加入supermaven

---
+++ lua/plugins/ai/supermaven.lua
@@ -0,0 +1,10 @@
+return {
+  "supermaven-inc/supermaven-nvim",
+  event = "InsertEnter",
+  opts = {
+    disable_inline_completion = true,
+    keymaps = {
+      accept_suggestion = nil,
+    },
+  },
+}
