9e2852ea3dac4850471fd36f1bf58ecb317dbc9e
houshanyun <likeredbean@gmail.com>
2026-04-23 07:11:51 +0800

    lsp 更新

---
+++ nvim/after/lsp/vtsls.lua
@@ -0,0 +1,21 @@
+return {
+  settings = {
+    typescript = {
+      preferences = {
+        includeCompletionsForModuleExports = true,
+        includeCompletionsWithInsertText = true,
+      },
+      suggest = {
+        autoImports = true,
+      },
+    },
+    javascript = {
+      preferences = {
+        includeCompletionsForModuleExports = true,
+      },
+      suggest = {
+        autoImports = true,
+      },
+    },
+  },
+}
