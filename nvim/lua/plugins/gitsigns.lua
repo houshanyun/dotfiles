5805c19d4d8de98a2df27e0100974b37e30142ff
houshanyun <likeredbean@gmail.com>
2026-04-25 09:15:01 +0800

    修改gitsigns

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,6 @@
 return {
   "lewis6991/gitsigns.nvim",
-  opt = {
+  opts = {
     signs                        = {
       add          = { text = '┃' },
       change       = { text = '┃' },
@@ -18,7 +18,7 @@ return {
       untracked    = { text = '┆' },
     },
     signs_staged_enable          = true,
-    signcolumn                   = true, -- Toggle with `:Gitsigns toggle_signs`
+    signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
     numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
     linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
     word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
@@ -40,7 +40,7 @@ return {
     blame_formatter              = nil, -- Use default
     sign_priority                = 6,
     update_debounce              = 100,
-    status_formatter             = nil, -- Use default
+    status_formatter             = nil,   -- Use default
     max_file_length              = 40000, -- Disable if file is longer than this (in lines)
     preview_config               = {
       -- Options passed to nvim_open_win
@@ -49,5 +49,65 @@ return {
       row = 0,
       col = 1
     },
-  }
+
+    on_attach                    = function(bufnr)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, desc)
+        vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+      end
+
+      -- Navigation：hunk 之�?跳�?
+      map('n', ']h', function()
+        if vim.wo.diff then
+          vim.cmd.normal({ ']c', bang = true })
+        else
+          gs.nav_hunk('next')
+        end
+      end, 'Next Hunk')
+
+      map('n', '[h', function()
+        if vim.wo.diff then
+          vim.cmd.normal({ '[c', bang = true })
+        else
+          gs.nav_hunk('prev')
+        end
+      end, 'Prev Hunk')
+
+      -- Stage / reset
+      map('n', '<leader>hs', gs.stage_hunk, 'Stage Hunk')
+      map('n', '<leader>hr', gs.reset_hunk, 'Reset Hunk')
+      map('v', '<leader>hs', function()
+        gs.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+      end, 'Stage Hunk (visual)')
+      map('v', '<leader>hr', function()
+        gs.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+      end, 'Reset Hunk (visual)')
+
+      map('n', '<leader>hS', gs.stage_buffer, 'Stage Buffer')
+      map('n', '<leader>hR', gs.reset_buffer, 'Reset Buffer')
+      map('n', '<leader>hu', gs.undo_stage_hunk, 'Undo Stage Hunk')
+
+      -- Preview / blame
+      map('n', '<leader>hp', gs.preview_hunk, 'Preview Hunk')
+      map('n', '<leader>hb', function()
+        gs.blame_line({ full = true })
+      end, 'Blame Line (full)')
+      map('n', '<leader>hB', gs.toggle_current_line_blame, 'Toggle Blame')
+
+      -- Diff
+      map('n', '<leader>hd', gs.diffthis, 'Diff This')
+      map('n', '<leader>hD', function()
+        gs.diffthis('~')
+      end, 'Diff This ~')
+
+      -- Toggle
+      map('n', '<leader>htd', gs.toggle_deleted, 'Toggle Deleted')
+      map('n', '<leader>htw', gs.toggle_word_diff, 'Toggle Word Diff')
+      map('n', '<leader>htn', gs.toggle_numhl, 'Toggle NumHL')
+
+      -- Text object：選??hunk
+      map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', 'Select Hunk')
+    end,
+  },
 }
